dcfbb3ba29f1045bf55ae6e19dd33c1b2d7b3a27
ff225 <frafranco16@gmail.com>
2026-05-14 10:51:11 +0200

    add esercizio1

---
+++ Lezione 6 - Tutorato/Studenti/esercizio1.c
@@ -0,0 +1,29 @@
+/*
+ * Esercizio 1 - Il primo programma multifile
+ *
+ * Obiettivo: scrivere un programma distribuito su due file sorgente.
+ *
+ * Compilazione:
+ *   gcc -Wall -Wextra -std=c11 esercizio1.c esercizio1_funzioni.c -o esercizio1
+ *
+ * Output atteso:
+ *   Il valore di a e': 42
+ */
+
+#include <stdio.h>
+
+/* TODO: definire qui la variabile globale "int a"
+ * (basta una sola riga, fuori da ogni funzione)
+ */
+
+/* TODO: dichiarare il prototipo della funzione stampa()
+ * che e' definita in funzioni.c
+ */
+
+int main(void) {
+  /* TODO: assegnare ad "a" il valore 42 */
+
+  /* TODO: chiamare la funzione stampa() */
+
+  return 0;
+}
---
+++ Lezione 6 - Tutorato/Studenti/esercizio1_funzioni.c
@@ -0,0 +1,20 @@
+/*
+ * Esercizio 1 - File funzioni.c
+ *
+ * Questo file definisce la funzione stampa() che usa la variabile
+ * globale "a" definita in esercizio1.c.
+ *
+ */
+
+#include <stdio.h>
+
+/* TODO: dichiarare qui che esiste una variabile "int a"
+ * definita altrove (in un altro file).
+ * Suggerimento: serve la parola chiave extern.
+ */
+
+void stampa(void) {
+  /* TODO: stampare il valore di a con printf,
+   * usando la stringa di formato: "Il valore di a e': %d\n"
+   */
+}
